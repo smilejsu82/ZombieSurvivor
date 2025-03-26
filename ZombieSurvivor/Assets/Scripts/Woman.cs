e6e0baa83b21c8614a27c291a56585e2d7c9256a
smilejsu82 <smilejsu82@gmail.com>
2025-03-26 16:35:12 +0900

    Woman Script 수정
    ^____^

--- ZombieSurvivor/Assets/Scripts/Woman.cs
+++ ZombieSurvivor/Assets/Scripts/Woman.cs
@@ -7,7 +7,7 @@ public class Woman : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log("Hello World ^________^");
     }
 
     // Update is called once per frame
