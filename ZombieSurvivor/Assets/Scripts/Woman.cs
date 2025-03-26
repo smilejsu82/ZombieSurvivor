ad6f5f7a4ddbc5b107b86150f5860ab9792f948e
smilejsu82 <smilejsu82@gmail.com>
2025-03-26 16:46:46 +0900

    스크립트 수정
    Hello Home

--- ZombieSurvivor/Assets/Scripts/Woman.cs
+++ ZombieSurvivor/Assets/Scripts/Woman.cs
@@ -7,7 +7,7 @@ public class Woman : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World Academy!!!");
+        Debug.Log("Hello Home!!!");
     }
 
     // Update is called once per frame
